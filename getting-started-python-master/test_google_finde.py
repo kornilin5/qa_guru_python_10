729d9f41783cb7753fd2b03d2afc0766d2a36fb0
Kornilin5 <sasha_kornilin@mail.ru>
2024-01-16 15:35:23 +0400

    new comment

--- getting-started-python-master/test_google_finde.py
+++ getting-started-python-master/test_google_finde.py
@@ -9,3 +9,4 @@ def test_search_text_two(options_browser):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('asdawerrgjsokglwkjkgnfahdadjaisdoajdsoajdhadasdasdadsad12313qawadsasad').press_enter()
     browser.element('#botstuff > div > div.mnr-c > div > p:nth-child(1)').should(have.text('ничего не найдено.'))
+    #testnewbranch
